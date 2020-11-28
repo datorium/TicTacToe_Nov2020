e70b58c57e73bfe1b28c402020bb7880319587e9
Datorium <hello@datorium.lv>
2020-11-28 12:17:52 +0200

    version 12:17

--- Form1.cs
+++ Form1.cs
@@ -13,6 +13,8 @@ namespace TicTacToe_Nov2020
     public partial class Form1 : Form
     {
         private string turn = "X";
+        private int turnCounter = 0;
+        private bool gameOver = false;
 
         public Form1()
         {
@@ -21,8 +23,12 @@ namespace TicTacToe_Nov2020
 
         private void PictureBox_Click(object sender, EventArgs e)
         {
+            if (gameOver)
+            {
+                return;
+            }
+            
             var picture = (PictureBox)sender;
-
             picture.SizeMode = PictureBoxSizeMode.StretchImage;
             
             if((string)picture.Tag == "X" || (string)picture.Tag == "O")
@@ -34,6 +40,7 @@ namespace TicTacToe_Nov2020
             {
                 picture.Image = Properties.Resources.x;
                 picture.Tag = turn;
+                turnCounter += 1;
                 CheckWinner();
                 turn = "O";
             }
@@ -41,6 +48,7 @@ namespace TicTacToe_Nov2020
             {
                 picture.Image = Properties.Resources.o;
                 picture.Tag = turn;
+                turnCounter += 1;
                 CheckWinner();
                 turn = "X";
             }
@@ -48,8 +56,44 @@ namespace TicTacToe_Nov2020
 
         private void CheckWinner()
         {
-            if(pictureBox1.Tag == pictureBox2.Tag && pictureBox2.Tag == pictureBox3.Tag)
+            if (pictureBox1.Tag == pictureBox2.Tag && pictureBox2.Tag == pictureBox3.Tag && pictureBox1.Tag != null)
             {
+                HighlightCells(pictureBox1, pictureBox2, pictureBox3);
+                GameOver();
+            }
+            else if (pictureBox4.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox6.Tag && pictureBox5.Tag != null)
+            {
+                HighlightCells(pictureBox4, pictureBox5, pictureBox6);
+                GameOver();
+            }
+            else if (pictureBox7.Tag == pictureBox8.Tag && pictureBox8.Tag == pictureBox9.Tag && pictureBox8.Tag != null)
+            {
+                HighlightCells(pictureBox7, pictureBox8, pictureBox9);
+                GameOver();
+            }
+            else if (pictureBox1.Tag == pictureBox4.Tag && pictureBox4.Tag == pictureBox7.Tag && pictureBox4.Tag != null)
+            {
+                HighlightCells(pictureBox1, pictureBox4, pictureBox7);
+                GameOver();
+            }
+            else if (pictureBox2.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox8.Tag && pictureBox5.Tag != null)
+            {
+                HighlightCells(pictureBox2, pictureBox5, pictureBox8);
+                GameOver();
+            }
+            else if (pictureBox3.Tag == pictureBox6.Tag && pictureBox6.Tag == pictureBox9.Tag && pictureBox6.Tag != null)
+            {
+                HighlightCells(pictureBox3, pictureBox6, pictureBox9);
+                GameOver();
+            }
+            else if (pictureBox1.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox9.Tag && pictureBox5.Tag != null)
+            {
+                HighlightCells(pictureBox1, pictureBox5, pictureBox9);
+                GameOver();
+            }
+            else if (pictureBox3.Tag == pictureBox5.Tag && pictureBox5.Tag == pictureBox7.Tag && pictureBox3.Tag != null)
+            {
+                HighlightCells(pictureBox3, pictureBox5, pictureBox7);
                 GameOver();
             }
         }
@@ -57,6 +101,13 @@ namespace TicTacToe_Nov2020
         private void GameOver()
         {
             MessageBox.Show("The winner is " + turn);
+            gameOver = true;
         }
+
+        private void HighlightCells(PictureBox pb1, PictureBox pb2, PictureBox pb3)
+        {
+            pb1.BackColor = pb2.BackColor = pb3.BackColor = Color.Blue;
+        }
+
     }
 }
